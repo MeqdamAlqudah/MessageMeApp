acb37eae544ae2497d46afcacb873e06211aa006
MeqdamAlqudah <memuq56@gmail.com>
2022-09-18 22:41:39 +0300

    remove code from boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -2,8 +2,3 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-at_exit do
-  Session.all.each do |session|
-    session.destroy
-  end
-end
