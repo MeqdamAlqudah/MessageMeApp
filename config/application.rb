e5ae4625f972dd11f3ba07041d278a2c92eb171c
MeqdamAlqudah <memuq56@gmail.com>
2022-09-19 20:21:44 +0300

    Try to fix deploy

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module RailsReactTutorial
     config.action_controller.default_protect_from_forgery = true
     config.action_cable.disable_request_forgery_protection = true
     config.action_cable.url = 'ws://localhost:6379/cable'
-    config.assets.initialize_on_precompile = false
 
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
