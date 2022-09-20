c81eadac3be9467842e8bac4e0cee573716530b9
MeqdamAlqudah <memuq56@gmail.com>
2022-09-20 13:14:19 +0300

    try to connect with redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,1 @@
-# $redis = Redis.new(url:  ENV['REDIS_URL'],
-#                           port: ENV['REDIS_PORT'],
-#                           db:   ENV['REDIS_DB'])
+$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
