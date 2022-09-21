eaebae8c91220df2b2655bd2913416d4ebbe1ff2
MeqdamAlqudah <memuq56@gmail.com>
2022-09-21 11:26:23 +0300

    Try to deploy my app with render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,9 @@
 set -o errexit
 
 bundle install
+rails webpacker:install
+rails webpacker:install:react 
+rails generate react
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
