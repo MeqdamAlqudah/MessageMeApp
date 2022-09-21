61ca7a2b89a62bd2a5167ad3921954b671533efb
MeqdamAlqudah <memuq56@gmail.com>
2022-09-21 11:09:18 +0300

    Try to deploy my app with render

--- config/puma.rb
+++ config/puma.rb
@@ -37,6 +37,12 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
 # preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
