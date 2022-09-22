d41452decc631392d1e7833142a2d807d149af20
MeqdamAlqudah <memuq56@gmail.com>
2022-09-22 12:52:15 +0300

    fix linter errors

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,9 +9,7 @@ module ApplicationCable
     private
 
     def find_verified_user
-      if verified_user = User.find_by(id: session[:user_id])
-        verified_user
-      end
+      verified_user if verified_user == User.find_by(id: session[:user_id])
     end
 
     def session
