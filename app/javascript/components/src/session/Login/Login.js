dd87a3bb2526209263786d51026b88266eefec59
MeqdamAlqudah <memuq56@gmail.com>
2022-09-26 14:16:07 +0300

    try to fix deployment

--- app/javascript/components/src/session/Login/Login.js
+++ app/javascript/components/src/session/Login/Login.js
@@ -37,7 +37,7 @@ class Login extends React.Component {
         // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: JSON.stringify({
-          username: this.usernameRef.current.value.replace(/\s/g, ''),
+          username: this.usernameRef.current.value.trim(),
           password: this.passwordRef.current.value,
         }),
       });
